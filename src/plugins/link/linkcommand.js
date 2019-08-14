c258f758d30a390a49b9025d7a8580b44480319b
Alexander Kiseliov <kiselev20060@gmail.com>
2019-08-14 18:23:23 +0300

    Added support for sequentially adding links.

--- src/plugins/link/linkcommand.js
+++ src/plugins/link/linkcommand.js
@@ -1,58 +1,66 @@
-import LinkCommand from "@ckeditor/ckeditor5-link/src/linkcommand";
-import findLinkRange from "@ckeditor/ckeditor5-link/src/findlinkrange";
+import LinkCommand from '@ckeditor/ckeditor5-link/src/linkcommand';
+import findLinkRange from '@ckeditor/ckeditor5-link/src/findlinkrange';
 import toMap from '@ckeditor/ckeditor5-utils/src/tomap';
 
 const defaultExecute = LinkCommand.prototype.execute;
-LinkCommand.prototype.execute = function (href, text, isCustom) {
-	if (!isCustom) {
-		return defaultExecute.apply(this, [href]);
+LinkCommand.prototype.execute = function( href, text, isCustom ) {
+	if ( !isCustom ) {
+		return defaultExecute.apply( this, [ href ] );
 	}
 	const model = this.editor.model;
 	const selection = model.document.selection;
 
-	model.change(writer => {
+	model.change( writer => {
 		// If selection is collapsed then update selected link or insert new one at the place of caret.
-		if (selection.isCollapsed) {
+		if ( selection.isCollapsed ) {
 			const position = selection.getFirstPosition();
 
 			// When selection is inside text with `linkHref` attribute.
-			if (selection.hasAttribute('linkHref')) {
+			if ( selection.hasAttribute( 'linkHref' ) ) {
 				// Then update `linkHref` value.
-				const linkRange = findLinkRange(selection.getFirstPosition(), selection.getAttribute('linkHref'), model);
+				const linkRange = findLinkRange( selection.getFirstPosition(), selection.getAttribute( 'linkHref' ), model );
 
-				writer.setAttribute('linkHref', href, linkRange);
+				writer.setAttribute( 'linkHref', href, linkRange );
 
 				// Create new range wrapping changed link.
-				writer.setSelection(linkRange);
+				writer.setSelection( linkRange );
 			}
 			// If not then insert text node with `linkHref` attribute in place of caret.
 			// However, since selection in collapsed, attribute value will be used as data for text node.
 			// So, if `href`/ 'text' is empty, do not create text node.
-			else if (text !== '' || href !== '') {
+			else if ( text !== '' || href !== '' ) {
+				const attributes = toMap( selection.getAttributes() );
 
-				const attributes = toMap(selection.getAttributes());
+				attributes.set( 'linkHref', href );
+				// add span with custom css class to indicate that link is inserted by custom outer action
+				attributes.set( 'customCssClass', true );
 
-				attributes.set('linkHref', href);
-				//add span with custom css class to indicate that link is inserted by custom outer action
-				attributes.set('customCssClass', true);
+				const node = writer.createText( text ? text : href, attributes );
 
-				const node = writer.createText(text ? text : href, attributes);
-
-				writer.insert(node, position);
+				writer.insert( node, position );
 
 				// Create new range wrapping created node.
-				writer.setSelection(writer.createRangeOn(node));
+				writer.setSelection( writer.createRangeOn( node ) );
+
+				// Create new paragraph after link to clear selection
+				const root = this.editor.model.document.getRoot();
+				const paragraph = writer.createElement( 'paragraph' );
+
+				writer.append( paragraph, root );
+				writer.setSelection( paragraph, 'in' );
+
+				this.editor.editing.view.focus();
 			}
 		} else {
 			// If selection has non-collapsed ranges, we change attribute on nodes inside those ranges
 			// omitting nodes where `linkHref` attribute is disallowed.
-			const ranges = model.schema.getValidRanges(selection.getRanges(), 'linkHref');
+			const ranges = model.schema.getValidRanges( selection.getRanges(), 'linkHref' );
 
-			for (const range of ranges) {
-				writer.setAttribute('linkHref', href, range);
+			for ( const range of ranges ) {
+				writer.setAttribute( 'linkHref', href, range );
 			}
 		}
-	});
+	} );
 };
 
 export default LinkCommand;
